f170240c576edceda89426cc703f25d7b160e684
wujj0326 <wuuj0326@gmail.com>
2019-09-12 17:00:03 -0700

    word

---
+++ datacamp_matlib.py
@@ -0,0 +1,10 @@
+#differences between Numpy array and list
+#      Numpy array                         List
+# All elements must be same type      Can mix types
+# (+)does element-wise addition       (+)concatenates lists
+# Multidimensional                    Single dimension
+# Range&Boolean indexing              Only Range indexing
+
+#Need to do math: Numpy array
+#Storing complex structures: List
+#Multidimensional: Numpy array
