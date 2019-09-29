dfc21e911918ba95dbfcd4f2eac29bb7e6442bc5
wujj0326 <wuuj0326@gmail.com>
2019-09-29 14:12:49 -0700

    'gg'

--- TriangleChallenge.py
+++ TriangleChallenge.py
@@ -1,52 +1,51 @@
 import turtle
+import math
 
+# set radius and theta
 radius = 200
-length = radius * 1.7321
 theta=1
-def triangle(col,x):
-    radius = 200
-    length = radius * 1.7321
-    for i in range(x):
-        trian.color(col)
-        trian.right(30)
-        trian.penup()
-        trian.backward(radius)
-        trian.left(theta)
-        radius=radius*0.9709
-        length=radius*1.7321
-        trian.forward(radius)
-        trian.left(150)
-        trian.pendown()
-        trian.forward(length)
-        for i in range(2):
-            trian.left(120)
-            trian.forward(length)
+# calculate rates
+lr_rate = 3/math.sqrt(3)
+new_r_rate = 0.5/math.sin(math.radians(150-theta))
+length = radius * lr_rate
 
 # Draw the first triangle
+def tri_1():
+    trian.penup()
+    trian.left(90)
+    trian.forward(radius)
+    trian.left(150)
+    trian.pendown()
+
+    trian.forward(length)
+    for i in range(2):
+        trian.left(120)
+        trian.forward(length)
+
+# Draw the inner triangle
+def triangle(radius, length):
+    for i in range(30):
+        c = ['red','orange','yellow','green','blue']
+        for i in c:
+            trian.color(i)
+            trian.right(30)
+            trian.penup()
+            trian.backward(radius)
+            trian.left(theta)
+            radius=radius*new_r_rate
+            length=radius*lr_rate
+            trian.forward(radius)
+            trian.left(150)
+            trian.pendown()
+            trian.forward(length)
+            for i in range(2):
+                trian.left(120)
+                trian.forward(length)
+    #return radius,length
 
 trian = turtle.Turtle()
 trian.speed(0)
 trian.color("red")
-trian.penup()
-trian.left(90)
-trian.forward(radius)
-trian.left(150)
-trian.pendown()
-
-trian.forward(length)
-for i in range(2):
-    trian.left(120)
-    trian.forward(length)
-# Draw the inner triangle
-triangle("red",150)
-
-# for i in range(20):
-#     triangle("red",2)
-#     triangle("orange",2)
-    # triangle("yellow",radius,length)
-    # triangle("green",radius,length)
-    # triangle("blue",radius,length)
-    # triangle("darkblue",radius,length)
-    # triangle("purple",radius,length)
-
+tri_1()
+triangle(radius,length)
 turtle.done()
